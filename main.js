6322d7f1abbc3b1ded95ab079a8cb397127f8c5d
segressio <52033804+segressio@users.noreply.github.com>
2020-05-22 11:32:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const todoControl = document.querySelector (".todo-control"),
+todoList = document.querySelector (".todo-list"),
+todoCompleted = document.querySelector (".todo-completed"),
+todoContainer = document.querySelector (".todo-container");
+
+let obj = [
+{
+value: "Сварить кофе",
+completed: false
+},
+
+{
+value: "Помыть посуду",
+completed: true
+}
+];
+
+const render = () =>{
+todoList.textContent="";
+todoCompleted.textContent="";
+let idCount=0;
+obj.forEach((el) =>{
+if (el!=null){
+const li = document.createElement("li");
+li.classList.add("todo-item");
+li.id=idCount;
+
+li.innerHTML = `<span class="text-todo">${el.value}</span>
+<div class="todo-buttons">
+<button class="todo-remove"></button>
+<button class="todo-complete"></button>
+</div>`;
+if (el.completed) todoCompleted.append(li);
+else todoList.append(li);
+}idCount++;
+})
+}
+render();
+
+todoControl.addEventListener("submit", (event) =>{
+event.preventDefault();
+const input = todoControl.querySelector("input");
+newObj = {value: input.value, completed: false}
+obj.push(newObj);
+render();
+save();
+} )
+
+
+
+todoContainer.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const target=event.target;
+    if (!target.matches('button'))return;
+    const id=parseInt(target.closest('li').id);
+    if (target.matches('.todo-remove')){
+        target.closest('li').remove();
+
+      delete  obj[id];
+    }
+    if (target.matches('.todo-complete')){
+        obj[id].completed=true;
+      render();
+
+    }
+    save();
+    })
+    function save()
+    {
+        const saveObj = JSON.stringify(obj);
+        localStorage.setItem("spisok",saveObj);
+    }
+    function load()
+    {
+        const loadObj =localStorage.getItem("spisok");
+        if (loadObj!=null){
+
+        obj = JSON.parse(loadObj);
+        }
+        render();
+    }
+    load();
+    
